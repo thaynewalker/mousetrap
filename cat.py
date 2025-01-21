67b774d2d87215417985afaaa9ae3d62ab43e00d
Thayne <kikansenkyoshi@hotmail.com>
2025-01-21 15:42:55 -0700

    Update cat.py replace tab char

--- cat.py
+++ cat.py
@@ -9,7 +9,7 @@ class cat(pygame.sprite.Sprite):
         
         # Pass in the color of the car, and its x and y position, width and height.
         # Set the background color and set it to be transparent
-	self.image = pygame.image.load("game/cat.png").convert_alpha()
+        self.image = pygame.image.load("game/cat.png").convert_alpha()
  
         # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
